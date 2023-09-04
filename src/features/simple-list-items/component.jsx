d5be9775dc88a5ce399e9b7a02234a5250dbc3cd
Mkxm Awilix <mkxm@outlook.fr>
2023-09-04 13:52:01 +0200

    [ADD] Added a Sort feature for the simple List component

--- src/features/simple-list-items/component.jsx
+++ src/features/simple-list-items/component.jsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import React from "react";
 import PropTypes from "prop-types";
 import { v4 as uuidv4 } from "uuid";
+import { sortBy } from "lodash";
 import {
     StyledButtonLarge,
     StyledButtonSmall,
@@ -10,29 +11,31 @@ import {
     StyledHeadlinePrimary,
 } from "../../components/styled-components";
 import { ReactComponent as Cross } from "../../icons/cross.svg";
+import { ReactComponent as ArrowUp } from "../../icons/arrow-up.svg";
+import { ReactComponent as ArrowDown } from "../../icons/arrow-down.svg";
 
 const initialItems = [
     {
         id: uuidv4(),
-        name: "Player 1",
+        name: "John Doe",
         points: 100,
         gold: 1500,
     },
     {
         id: uuidv4(),
-        name: "Player 2",
+        name: "Mary Doe",
         points: 70,
         gold: 850,
     },
     {
         id: uuidv4(),
-        name: "Player 3",
+        name: "Alice Doe",
         points: 40,
         gold: 2400,
     },
     {
         id: uuidv4(),
-        name: "Player 4",
+        name: "Tom Doe",
         points: 30,
         gold: 550,
     },
@@ -98,19 +101,52 @@ const SimpleListItems = () => {
     );
 };
 
-const List = React.memo(({ items, onRemoveItem }) => (
-    <div>
-        <li className="item item-header">
-            <span>Name</span>
-            <span>Points</span>
-            <span>Gold</span>
-            <span>Action</span>
-        </li>
-        {items.map((item) => (
-            <Item key={item.id} item={item} onRemoveItem={onRemoveItem} />
-        ))}
-    </div>
-));
+const SORTS = {
+    NONE: (list) => list,
+    NAME: (list) => sortBy(list, "name"),
+    POINT: (list) => sortBy(list, "points").reverse(),
+    GOLD: (list) => sortBy(list, "gold").reverse(),
+}
+
+const List = React.memo(({ items, onRemoveItem }) => {
+    
+    const [sort, setSort] = React.useState({ sortKey: "NONE", isReverse: false });
+    const handleSort = (sortKey) => {
+        const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+        setSort({ sortKey, isReverse: isReverse });
+    };
+    const sortFunction = SORTS[sort.sortKey];
+    const sortedList = sort.isReverse ? sortFunction(items).reverse() : sortFunction(items);
+
+    return (
+        <div>
+            <li className="item item-header">
+                <span>
+                    <button type="button" onClick={() => handleSort("NAME")}>
+                        Name
+                        {sort.sortKey == "NAME" ? (sort.isReverse ? <ArrowUp height="18px" width="18px" /> : <ArrowDown height="18px" width="18px" />) : null}
+                    </button>
+                </span>
+                <span>
+                    <button type="button" onClick={() => handleSort("POINT")}>
+                        Points
+                        {sort.sortKey == "POINT" ? (sort.isReverse ? <ArrowUp height="18px" width="18px" /> : <ArrowDown height="18px" width="18px" />) : null}
+                    </button>
+                </span>
+                <span>
+                    <button type="button" onClick={() => handleSort("GOLD")}>
+                        Gold
+                        {sort.sortKey == "GOLD" ? (sort.isReverse ? <ArrowUp height="18px" width="18px" /> : <ArrowDown height="18px" width="18px" />) : null}
+                    </button>
+                </span>
+                <span>Action</span>
+            </li>
+            {sortedList.map((item) => (
+                <Item key={item.id} item={item} onRemoveItem={onRemoveItem} />
+            ))}
+        </div>
+    );
+});
 List.displayName = "List";
 
 const Item = ({ item, onRemoveItem }) => (
